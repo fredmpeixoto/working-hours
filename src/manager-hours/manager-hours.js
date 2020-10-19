0dcdbbc08ab33a39447cae7863685efedf9bb42c
Fred Peixoto <fred.peixoto@softwillians.com.br>
2020-10-19 18:48:34 -0300

    Add material UI icons and SCSS

---
+++ src/manager-hours/manager-hours.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './manager-hours.scss';
+
+function ManagerHours() {
+    return (
+        <div>
+
+            <h1> Ola cheguei </h1>
+        </div>
+    );
+}
+
+export default ManagerHours;
