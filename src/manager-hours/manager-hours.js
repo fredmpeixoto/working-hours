f3d58c0b65d4c4c1ec97cdb6c7f9bb433ce661f9
Fred Peixoto <fred.peixoto@softwillians.com.br>
2020-10-19 20:06:53 -0300

    function clock

--- src/manager-hours/manager-hours.js
+++ src/manager-hours/manager-hours.js
@@ -2,14 +2,56 @@ import React from 'react';
 import './manager-hours.scss';
 import Icon from '@material-ui/core/Icon';
 
+class ManagerHours extends React.Component {
 
-function ManagerHours() {
-    return (
-        <div className="icon">
-            <Icon >play_arrow</Icon>
-            <h1> Ola cheguei </h1>
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true, iconPause: 'pause' };
+        this.handleClick = this.handleClick.bind(this);
+        this.showLunch = this.showLunch.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({ isToggleOn: !state.isToggleOn }));
+    }
+
+    getIconStatus() {
+        return this.state.isToggleOn ? 'play_arrow' : this.state.iconPause;
+    }
+
+    getIconLunch() {
+        return this.state.iconPause === 'fastfood' ? 'pause' : 'fastfood';
+    }
+
+    showLunch() {
+        this.setState((state) => ({ iconPause: this.state.iconPause === 'fastfood' ? 'pause' : 'fastfood' }))
+    }
+
+    render() {
+        return (
+            <div>
+
+                <div className="icon">
+
+                    <Icon onClick={this.handleClick} >
+                        {this.getIconStatus()}
+                    </Icon>
+
+                    <div className="food">
+
+                        <Icon onClick={this.showLunch}>
+                            {this.getIconLunch()}
+                        </Icon>
+
+                    </div>
+
+                </div>
+
+                <h1>  Ola cheguei </h1>
+            </div>
+        );
+    }
 }
+
 
 export default ManagerHours;
