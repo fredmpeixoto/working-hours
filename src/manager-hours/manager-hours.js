4801386be6624ad020e1a3005df792fd3a04579e
Fred Peixoto <fred.peixoto@softwillians.com.br>
2020-10-22 13:01:11 -0300

    create total time

--- src/manager-hours/manager-hours.js
+++ src/manager-hours/manager-hours.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import moment from 'moment';
+
 import Icon from '@material-ui/core/Icon';
+import Divider from "@material-ui/core/Divider";
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 import './manager-hours.scss';
 import RequestService from '../services/request.services';
@@ -14,9 +25,12 @@ class ManagerHours extends React.Component {
             iconWork: 'play_arrow',
             iconLunch: 'fastfood',
             textInfo: '', name: '',
+            sumTotalSegWorking: '',
             hours: [{
                 start: '',
-                end: ''
+                end: '',
+                kindBreak: '',
+                total: ''
             }]
         };
 
@@ -24,10 +38,11 @@ class ManagerHours extends React.Component {
         this.requestHours = this.requestHours.bind(this);
         this.getDataFromUser = this.getDataFromUser.bind(this);
 
+        this.onToggleTable = this.onToggleTable.bind(this);
         this.handleClickWork = this.handleClickWork.bind(this);
         this.handleClickLunch = this.handleClickLunch.bind(this);
+        this.setTotalHoursWork = this.setTotalHoursWork.bind(this);
     }
-
 
     componentDidMount() {
         this.getDataFromUser();
@@ -42,28 +57,31 @@ class ManagerHours extends React.Component {
 
     updateHours(kindBreak) {
 
-        var circlesComplete = this.state.hours.every(hour => (hour.start && hour.end));
+        let cyclesComplete = true;
 
-        const lastHourCircle = this.state.hours[this.state.hours.length - 1];
+        let notIsSomeKindBreak = false;
 
-        var notIsSomeKindBreak = false;
+        const lastHourCycle = this.state.hours[this.state.hours.length - 1];
 
-        if (lastHourCircle)
-            notIsSomeKindBreak = (lastHourCircle.kindBreak !== kindBreak);
+        if (lastHourCycle) {
+            cyclesComplete = (lastHourCycle.start && lastHourCycle.end);
+            notIsSomeKindBreak = (lastHourCycle.kindBreak !== kindBreak);
+        }
 
-        if (circlesComplete)
-            this.createNewCircle(kindBreak);
+        if (cyclesComplete)
+            this.createNewCycle(kindBreak);
         else if (notIsSomeKindBreak) {
-            this.setHoursStartOrEnd(lastHourCircle, lastHourCircle.kindBreak);
-            this.createNewCircle(kindBreak);
+            this.setHoursStartOrEnd(lastHourCycle, lastHourCycle.kindBreak);
+            this.createNewCycle(kindBreak);
         } else
-            this.setHoursStartOrEnd(lastHourCircle, kindBreak);
+            this.setHoursStartOrEnd(lastHourCycle, kindBreak);
 
+        this.setTotalHoursWork();
 
         this.requestHours();
     }
 
-    createNewCircle(kindBreak) {
+    createNewCycle(kindBreak) {
         this.state.hours.push({ start: '', end: '' });
         this.setHoursStartOrEnd(this.state.hours[this.state.hours.length - 1], kindBreak);
     }
@@ -78,11 +96,14 @@ class ManagerHours extends React.Component {
 
         hour.kindBreak = kindBreak;
 
-        var startedCircleButNotEnd = (hour.start && !hour.end);
+        var startedCycleButNotEnd = (hour.start && !hour.end);
 
-        if (!hour.start) hour.start = new Date().toLocaleTimeString();
+        if (!hour.start) hour.start = new Date().toLocaleString();
 
-        else if (startedCircleButNotEnd) hour.end = new Date().toLocaleTimeString();
+        else if (startedCycleButNotEnd) {
+            hour.end = new Date().toLocaleString();
+            hour.total = moment(hour.end).diff(moment(hour.start), 'second')
+        }
     }
 
     handleClickWork() {
@@ -119,39 +140,111 @@ class ManagerHours extends React.Component {
         return this.state.iconWork === 'play_arrow' ? 'Take time to breath, Follow the circle.' : '';
     }
 
+    onToggleTable() {
+        this.setState((state) => ({ showTable: !state.showTable }));
+    }
+
+    setTotalHoursWork() {
+        let timesWorking = this.state.hours.filter(hr => hr.kindBreak === 'work');
+
+        if (timesWorking.length === 0) return;
+
+        const sumTotalSegWorking = timesWorking.reduce((accumulator, time) => accumulator + time.total, 0);
+
+        var formatted = moment.utc(sumTotalSegWorking * 1000).format('HH:mm:ss');
+
+        formatted = formatted === 'Invalid date' ? this.state.sumTotalSegWorking : formatted; 
+
+        this.setState(() => ({ sumTotalSegWorking: formatted }));
+    }
+
 
 
     render() {
         return (
-            <div className="default">
+            <div>
 
-                <p>Hello {this.state.name}</p>
+                <div className="default">
 
-                <div className="icon">
+                    <p>Hello {this.state.name}</p>
+
+                    <div className="icon">
 
 
-                    <Icon className={this.getClassBreath()} onClick={this.handleClickWork} >
-                        {this.state.iconWork}
-                    </Icon>
-
-                    <div className="lunch">
-
-                        <Icon onClick={this.handleClickLunch}>
-                            {this.state.iconLunch}
+                        <Icon className={this.getClassBreath()} onClick={this.handleClickWork} >
+                            {this.state.iconWork}
                         </Icon>
+
+                        <div className="lunch">
+
+                            <Icon onClick={this.handleClickLunch}>
+                                {this.state.iconLunch}
+                            </Icon>
+
+                        </div>
 
                     </div>
 
+                    <h3>
+                        {this.state.textInfo}
+                    </h3>
+                    <p>
+                        <small>
+                            {this.showMessage()}
+                        </small>
+                    </p>
                 </div>
 
-                <h3>
-                    {this.state.textInfo}
-                </h3>
-                <p>
-                    <small>
-                        {this.showMessage()}
-                    </small>
-                </p>
+                <Divider />
+
+                <div className='info-hours'>
+
+                    <ul>
+                        <li>
+                            Arrival : {this.state.hours[0]?.start || 0}
+                        </li>
+
+                        <li>
+                            Last Exiting : {this.state.hours[this.state.hours.length - 1]?.end}
+                        </li>
+
+                        <li>
+                            Stop To Lunch : {this.state.hours[this.state.hours.length - 1]?.end}
+                        </li>
+
+                        <li>
+                            Total Work : {this.state.sumTotalSegWorking || 0}
+                        </li>
+                    </ul>
+
+                </div>
+
+
+                <Divider />
+
+                <p onClick={this.onToggleTable} className='cursor'>Show Hours</p>
+
+                <TableContainer component={Paper} className={this.state.showTable ? 'table-style' : 'hidden'}>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">Type Break</TableCell>
+                                <TableCell align="center">Start</TableCell>
+                                <TableCell align="center">End</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.hours.map((row) => (
+                                <TableRow key={row.start}>
+                                    <TableCell align="left">{row.kindBreak}</TableCell>
+                                    <TableCell align="right">{row.start}</TableCell>
+                                    <TableCell align="right">{row.end}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
             </div>
         );
     }
